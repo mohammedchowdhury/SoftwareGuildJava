f53a7fcae75ed18cdc71a7dda764e5d74f06c009
mohammedchowdhury <mohammedchowdhury60@gmail.com>
2020-05-01 16:57:16 -0400

    adding connect 4

--- MileStone2/src/main/java/connectFour/appBackup.java
+++ MileStone2/src/main/java/connectFour/appBackup.java
@@ -1,17 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package connectFour;
-
 import java.util.Scanner;
-
 /**
  *
  * @author mohammedchowdhury
  */
-public class app {
+import java.util.Scanner;
+public class appBackup {
      static String winner = ""; 
      static boolean won = false; 
      static String [][] arr = {{"_","_","_","_","_","_","_"},
@@ -26,9 +20,7 @@ public class app {
          System.out.println("1. An empty board"); 
          displayBoard(arr);
          
-         while(won==false){
-             
-             
+         while(won==false){        
              int x = getUserInputNumber("Choose a column: ");
              x--; 
              
@@ -37,29 +29,25 @@ public class app {
                  userInput(x, "X");
              }else{
                  System.out.println("Player one chooses column "+(x+1));
-                 userInput(x, "O");
-                 
+                 userInput(x, "O");              
              }
              counter++; 
-             System.out.println(" ");
-             
-      
+             System.out.println(" ");                  
         } 
-         
+  
          if(winner.equals("X")){
              System.out.println("Winner is X ");
          }
          else{
              System.out.println("Winner is 0");
          }
-         System.exit(0);
-        
+         System.exit(0); 
     }
      
      public static void validateLastInput(int x, int y){
          checkHorixontal(x,y); 
          checkVertical(x,y);   
-         
+        
          int t = Math.min(x, y);        
          int tX1 = x-t,tY1 = y-t; 
          while(tX1 != arr.length && tY1 != arr[0].length){
@@ -70,43 +58,34 @@ public class app {
          }
 
         int tX,tY; 
-        if(x+y<arr.length){
-           
+        if(x+y<arr.length){     
            tX = x+y;
            tY = 0; 
-           // System.out.println(tX+" "+tY);
         }
         else{
            int sum = x+y; 
            tX = arr.length-1;
            sum = sum-tX; 
            tY = sum; 
-           // System.out.println(tX+"  "+tY);
         }
 
         while(tX != -1 && tY!=arr[0].length){
-           //System.out.println(tX+" "+tY);
            checkDiagnalRightToLeftUp(tX,tY);
             tX--; 
             tY++; 
         }
-
-         
      }
      
     public static void checkHorixontal(int x, int y){
          String s1,s2,s3,s4;     
          for(int a=0; a<arr[0].length-3; a++){
-                    s1 = arr[x][a]; 
+             s1 = arr[x][a]; 
              s2 = arr[x][a+1]; 
              s3 = arr[x][a+2]; 
              s4 = arr[x][a+3]; 
-            // System.out.println(s1+" "+s2+" "+s3+" "+s4);
-             if(s1.equals(s2) && s2.equals(s3) && s3.equals(s4) && (s1.equals("_")==false) ){
-                
+             if(s1.equals(s2) && s2.equals(s3) && s3.equals(s4) && (s1.equals("_")==false) ){            
                  won  = true; 
                  winner = s1;
-                 //System.out.println(arr[x][a]+" "+arr[x][a+1]+" "+arr[x][a+2]+" "+arr[x][a+3]);
                  return; 
              }
          }
@@ -122,10 +101,7 @@ public class app {
              s2 = arr[x-1][y+1]; 
              s3 = arr[x-2][y+2]; 
              s4 = arr[x-3][y+3]; 
-             
-           
-             
-             //System.out.println(s1+" "+s2+" "+s3+" "+s4+"  ssss");
+
              if(s1.equals(s2) && s2.equals(s3) && s3.equals(s4) && (s1.equals("_")==false)){
                  won  = true; 
                  winner = s1;
@@ -140,30 +116,23 @@ public class app {
             return; 
         }
          String s1="",s2="",s3="",s4="";     
-//         //for(int a=0; a<arr[y].length-3; a++){
              s1 = arr[x][y]; 
              s2 = arr[x+1][y+1]; 
              s3 = arr[x+2][y+2]; 
              s4 = arr[x+3][y+3]; 
              
-             //System.out.println(s1+" "+s2+" "+s3+" "+s4);
              if(s1.equals(s2) && s2.equals(s3) && s3.equals(s4) && (s1.equals("_")==false)){
-                 // System.out.println("aa was hitt");     
+   
                  won  = true; 
                  winner = s1;
-                 //System.out.println(s1+" "+s2+" "+s3+" "+s4);
                  return; 
              }
     }
   
     public static void checkVertical(int x,int y){
         String s1,s2,s3,s4;        
-//         System.out.println(x+" x");
-//          System.out.println(y+" y");
-             
+        
          for(int a=arr.length-4; a>-1; a--){
-       
-        //for(int a=0; a<arr.length-3; a++){
             s1 = arr[a][y]; 
             s2 = arr[a+1][y]; 
             s3 = arr[a+2][y]; 
@@ -173,27 +142,23 @@ public class app {
                 return; 
             }
             
-           // System.out.println(arr[a][y]+" "+arr[a+1][y]+" "+arr[a+2][y]+" "+arr[a+3][y]);             
             if(s1.equals(s2) && s2.equals(s3) && s3.equals(s4) && (s1.equals("_")==false)){            
                 won  = true; 
                 winner = s1;
-//                System.out.println("Won");
-               //System.out.println(arr[a][y]+" "+arr[a+1][y]+" "+arr[a+2][y]+" "+arr[a+3][y]);
                 return; 
              }
          }
      }
     
-    public static  void userInput(int x, String st){ //puts userinput in the grid will need to get user input 1-7 and minus one 
-              
+    public static  void userInput(int x, String st){ //puts userinput in the grid will need to get user input 1-7 and minus one          
        if(arr[0][x].equals("_")==false){  // check to see if the column if full
            System.out.println("COLUMN IS FULL");
             return; 
         } 
-       if(arr[5][x].equals("_")){  // checks to see if the column is empty
-           arr[5][x] = st; 
-           //displayBoard(arr); 
-           validateLastInput(5, x);
+       
+       if(arr[arr.length-1][x].equals("_")){  // checks to see if the column is empty
+           arr[arr.length-1][x] = st; 
+           validateLastInput(arr.length-1, x);
            displayBoard(arr);
            return; 
        }
@@ -223,7 +188,6 @@ public class app {
                 }
             } catch (NumberFormatException ex) {
                 System.out.println("That was not a whole number!  " + ex);
-                // ex.printStackTrace();
             }
             System.out.println(number);
         } while (isValid == false);
@@ -244,43 +208,3 @@ public class app {
 }
     
 
-    ////////////////// Code under works well ///////////////
-     
-//    public static void checkDiagnalRightToLeftDown(int x, int y){ 
-//           if(x>2 || y<3){ // cannot check because we dont have a stacl greater then 3
-//            //System.out.println("Cannot check diag");
-//            return; 
-//        }
-//         String s1="",s2="",s3="",s4="";     // goign up form left corner to right corner
-//             s1 = arr[x][y]; 
-//             s2 = arr[x+1][y-1]; 
-//             s3 = arr[x+2][y-2]; 
-//             s4 = arr[x+3][y-3]; 
-//                    
-//             if(s1.equals(s2) && s2.equals(s3) && s3.equals(s4) && (s1.equals("_")==false)){
-//                 won  = true; 
-//                 winner = s1;
-//                 return; 
-//             }
-//    }
-     
-
-     
-//    public static void checkDiagnalLeftToRightUp(int x, int y){
-//        System.out.println(x+" "+y);
-//        if(x<3 || y<3){ // cannot check because we dont have a stacl greater then 3
-//            //System.out.println("Cannot check diag");
-//            return; 
-//        }
-//         String s1="",s2="",s3="",s4="";     
-//             s1 = arr[x][y]; 
-//             s2 = arr[x-1][y-1]; 
-//             s3 = arr[x-2][y-2]; 
-//             s4 = arr[x-3][y-3]; 
-//             
-//             if(s1.equals(s2) && s2.equals(s3) && s3.equals(s4) && (s1.equals("_")==false)){
-//                 won  = true; 
-//                 winner = s1;
-//                 return; 
-//             }
-//    }
